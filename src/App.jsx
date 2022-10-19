b722800220733d5dd873170cd80f44f02186fa81
muhammadkhalipha <yusufmuhammadbiodun@gmail.com>
2022-10-19 14:48:21 +0100

    https://github.com/muhammadkhalipha/User-Interface-Project.git

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mainDiv">
+      <div className="muhammadApp">
+        <h1> User Input User Output</h1>
+        <p> Hello User!!</p>
+        <input type="text" placeholder="Web Developer" />
+
+        <p> User Name: Web Developer </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
